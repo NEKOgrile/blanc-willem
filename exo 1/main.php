4f00a1df7f0a465ae94583c945fea3b7f508b982
willem <willemcornil@gmail.com>
2023-03-31 14:33:04 +0200

    je sais pas

--- exo 1/main.php
+++ exo 1/main.php
@@ -9,27 +9,36 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <body>
-    <input type="button" value="je sais pas" onclick="newrole()">
+    <div class="containeurelementnewrole">
+        <input type="button" value="je sais pas" onclick="newrole()">
 
-    <!-- les containeur qui aparaisent quand on clique sur un bouton -->
-    <div class="containeurnewrole" id="containeurnewroleid">
-        <form action="" method="POST">
-            <input class="item-boutton oui" type="submit" value="oui" name="newroleoui" />
-            <div class="containeurelementnewrole">
-                <p class="textprof">
-                    nom du nouveau rôle
-                </p>
-                <input type="text" name="nomrole" placeholder="ici nom du rôle">
-                <p class="textprof">
-                    description du rôle a ajouter
-                </p>
-                <textarea class="" value="" name="descriptionrole" placeholder="ici la description"></textarea>
-        </form>
+        <!-- les containeur qui aparaisent quand on clique sur un bouton -->
+        <div class="containeurnewrole" id="containeurnewroleid">
+            <table class="table">
+                <tr>
+                    <form action="" method="POST">
+                        <td>
+                            <input class="item-boutton oui" type="submit" value="oui" name="newroleoui" />
+                        </td>
+                        <td>
+                            <p class="textprof">
+                                nom du nouveau rôle
+                            </p>
+                            <input type="text" name="nomrole" placeholder="ici nom du rôle">
+                            <p class="textprof">
+                                description du rôle a ajouter
+                            </p>
+                            <textarea class="" value="" name="descriptionrole"
+                                placeholder="ici la description"></textarea>
+                        </td>
+                    </form>
+                    <td>
+                        <input class="item-boutton non" type="button" value="non" onclick="nonactionmodifierrole()" />
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
-
-    <input class="item-boutton non" type="button" value="non" onclick="nonactionmodifierrole()" />
-
-
 </body>
 
 <script type="text/javascript" src="main.js"></script>
