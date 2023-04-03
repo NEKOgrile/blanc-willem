6b8ad7f1b15b37ae5cb36353ef63b6c2acbfb31a
willem <willemcornil@gmail.com>
2023-04-03 11:30:03 +0200

    ajax controle MAJ

--- exo4/main2.php
+++ exo4/main2.php
@@ -10,12 +10,22 @@ header("Access-Control-Allow-Headers: Content-Type");
 if (isset($_POST['name']) && isset($_POST['email'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    echo "Nom: $name<br>";
-    echo "Email: $email<br>";
 
-    $registernewpp = $bdd->prepare(
-        'INSERT INTO ajax (nom , email) VALUE (?,?)'
-    );
-    $registernewpp->execute(array($name, $email));
+
+    $mail = $bdd->prepare('SELECT * FROM ajax WHERE email = ?');
+    $mail->execute([$email]);
+    $mailexist = $mail->rowCount();
+
+    if ($mailexist != 0) {
+        $reponseexiste = array("nom" => $name, "email" => $email, "erreur" => "l email existe deja");
+        echo ($json = json_encode($reponseexiste));
+    } else {
+        $registernewpp = $bdd->prepare(
+            'INSERT INTO ajax (nom , email) VALUE (?,?)'
+        );
+        $registernewpp->execute(array($name, $email));
+        echo json_encode("ok");
+    }
+
 }
 ?>
