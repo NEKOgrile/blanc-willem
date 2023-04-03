dbf4c2faa8ef7a3f627d41e5567a29130ae64f0e
willem <willemcornil@gmail.com>
2023-04-03 10:27:00 +0200

    MAJ

--- exo4/main2.php
+++ exo4/main2.php
@@ -4,11 +4,16 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-
+if (isset($_POST)) {
+    var_dump($_POST);
+    print_r($_POST);
+}
 if (isset($_POST['name']) && isset($_POST['email'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     echo "Nom: $name<br>";
     echo "Email: $email<br>";
+
+    echo "je sais pas";
 }
 ?>
